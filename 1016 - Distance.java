19b213b3ba61253693cb1404072540d2ac9c341a
Afrin Sultana Akhi <109916569+SnOpWoOt@users.noreply.github.com>
2023-06-13 02:13:02 +0600

    Create 1016 - Distance.java

---
+++ 1016 - Distance.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+//by_akhi
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+     //   int x = 60,y = 90;
+        int n = scanner.nextInt();
+        System.out.print(n*2);
+        System.out.println(" minutos");
+    }
+}
