7d91e2b903de6b58161f7723fe20f3426df047e4
Afrin Sultana Akhi <109916569+SnOpWoOt@users.noreply.github.com>
2023-12-10 19:20:13 +0600

    Add files via upload

---
+++ 1005 Average 1.py
@@ -0,0 +1,4 @@
+a = float(input())
+b = float (input())
+avg =( (a*3.5) +(b*7.5)) /(3.5+7.5)
+print(f'MEDIA = {avg:.5f}')
---
+++ 1006 Average 2.py
@@ -0,0 +1,5 @@
+a = float(input())*2
+b = float(input())*3
+c = float(input())*5
+avg = (a+b+c)/(2+3+5)
+print(f'MEDIA = {avg:.1f}')
