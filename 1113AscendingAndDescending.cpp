db30986240518eaaead5aff2a695678cc99ce045
Afrin Sultana Akhi <109916569+SnoOpWo0t@users.noreply.github.com>
2023-12-29 19:47:39 +0600

    Add files via upload

---
+++ 1113AscendingAndDescending.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int x,y;
+    
+    while(cin>>x>>y && x!=y)
+    {
+        if(x>y)
+            cout<<"Decrescente"<<endl;
+        else
+            cout<<"Crescente"<<endl;
+        cin>>x>>y;
+    }
+}
