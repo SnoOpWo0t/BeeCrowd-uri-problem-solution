420fb9d07f666aea51b85ac0d3830537d79a5f2e
Afrin Sultana Akhi <afrinsultanaakhi138@gmail.com>
2023-12-29 21:54:30 +0600

    k

---
+++ 1094 -Experiments.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    int q;
+    char s;
+    int total = 0;
+    int coelho = 0;
+    int sapo = 0;
+    int rato = 0;
+    cin >> n;
+    
+    for(int i = 0; i < n; i++){
+            cin >> q;
+            cin >> s;
+            
+            total += q;
+            
+            if(s == 'C') coelho += q;
+            if(s == 'S') sapo += q;
+            if(s == 'R') rato += q;
+    } 
+    cout << "Total: " << total << " cobaias\n";
+    cout << "Total de coelhos: " << coelho << "\n";
+    cout << "Total de ratos: " << rato << "\n";
+    cout << "Total de sapos: " << sapo << "\n";
+    printf("Percentual de coelhos: %.2f %\n", ((float)coelho/(float)total)*100.00);
+    printf("Percentual de ratos: %.2f %\n", ((float)rato/(float)total)*100.00);
+    printf("Percentual de sapos: %.2f %\n", ((float)sapo/(float)total)*100.00);
+
+    
+}
